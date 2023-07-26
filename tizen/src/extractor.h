4da091ce77959dfe71fb67b1d3a68e9f6dda959b
jhd0 <dojh99@gmail.com>
2023-07-26 11:12:11 +0900

    move constants to relevant files

--- tizen/src/extractor.h
+++ tizen/src/extractor.h
@@ -13,21 +13,55 @@
 
 typedef flutter::MethodCall<flutter::EncodableValue> FlMethodCall;
 
+const char *kInitialDelaySecondsKey = "initialDelaySeconds";
+const char *kBackOffPolicyTypeKey = "backoffPolicyType";
+const char *kBackOffPolicyDelayMillisKey = "backoffDelayInMilliseconds";
+const char *kOutofQuotaPolicyKey = "outOfQuotaPolicy";
+const char *kExistingWorkpolicykey = "existingWorkPolicy";
+
+const char *kNetworkTypekey = "networkType";
+const char *kBatteryNotLowKey = "requiresBatteryNotLow";
+const char *kChargingKey = "requiresCharging";
+const char *kDeviceidlekey = "requiresDeviceIdle";
+const char *kStorageNotLowKey = "requiresStorageNotLow";
+
+// NetworkType
+const char *kConnected = "CONNECTED";
+const char *kMetered = "METERED";
+const char *kNotRequired = "NOT_REQUIRED";
+const char *kNotRoaming = "NOT_ROAMING";
+const char *kUnmetered = "UNMETERED";
+const char *kTemporarilyUnmetered = "TEMPORARILY_UNMETERED";
+
+// ExistingWorkPolicy
+const char *kReplace = "REPLACE";
+const char *kKeep = "KEEP";
+const char *kAppend = "APPEND";
+const char *kUpdate = "UPDATE";
+
+// BackOffPolicy
+const char *kExponential = "EXPONENTIAL";
+const char *kLinear = "LINEAR";
+
+// OutofQuotaPolicy
+const char *kRunAsNonExpectedWorkRequest = "RUN_AS_NON_EXPEDITED_WORK_REQUEST";
+const char *kDropWorkRequest = "DROP_WORK_REQUEST";
+
 ExistingWorkPolicy ExtractExistingWorkPolicyFromMap(
     const flutter::EncodableMap &map) {
     std::string value;
     bool succeed = GetValueFromEncodableMap<std::string>(
-        &map, constants::keys::kExistingWorkpolicykey, value);
+        &map, kExistingWorkpolicykey, value);
     if (succeed) {
         Upper(value);
 
-        if (value == constants::values::kReplace) {
+        if (value == kReplace) {
             return ExistingWorkPolicy::kReplace;
-        } else if (value == constants::values::kKeep) {
+        } else if (value == kKeep) {
             return ExistingWorkPolicy::kKeep;
-        } else if (value == constants::values::kAppend) {
+        } else if (value == kAppend) {
             return ExistingWorkPolicy::kAppend;
-        } else if (value == constants::values::kUpdate) {
+        } else if (value == kUpdate) {
             return ExistingWorkPolicy::kUpdate;
         }
     }
@@ -37,21 +71,19 @@ ExistingWorkPolicy ExtractExistingWorkPolicyFromMap(
 std::optional<BackoffPolicyTaskConfig> ExtractBackoffPolicyConfigFromMap(
     const flutter::EncodableMap &map, TaskType task_type) {
     std::string value;
-    if (!GetValueFromEncodableMap(&map, constants::keys::kBackOffPolicyTypeKey,
-                                  value)) {
+    if (!GetValueFromEncodableMap(&map, kBackOffPolicyTypeKey, value)) {
         return std::nullopt;
     }
 
     BackoffPolicy backoff_policy;
-    if (value == constants::values::kExponential) {
+    if (value == kExponential) {
         backoff_policy = BackoffPolicy::kExponential;
     } else {
         backoff_policy = BackoffPolicy::kLinear;
     }
 
     int32_t requested_backoff_delay =
-        GetOrNullFromEncodableMap<int32_t>(
-            &map, constants::keys::kBackOffPolicyDelayMillisKey)
+        GetOrNullFromEncodableMap<int32_t>(&map, kBackOffPolicyDelayMillisKey)
             .value_or(15 * 6 * 1000) /
         1000;
     int32_t minimum_backoff_delay = task_type.minimum_backoff_delay;
@@ -66,8 +98,8 @@ std::optional<BackoffPolicyTaskConfig> ExtractBackoffPolicyConfigFromMap(
 
 std::optional<OutOfQuotaPolicy> ExtractOutOfQuotaPolicyFromMap(
     const flutter::EncodableMap &map) {
-    std::optional<std::string> value = GetOrNullFromEncodableMap<std::string>(
-        &map, constants::keys::kOutofQuotaPolicyKey);
+    std::optional<std::string> value =
+        GetOrNullFromEncodableMap<std::string>(&map, kOutofQuotaPolicyKey);
 
     if (!value.has_value()) {
         return std::nullopt;
@@ -75,9 +107,9 @@ std::optional<OutOfQuotaPolicy> ExtractOutOfQuotaPolicyFromMap(
 
     Upper(value.value());
 
-    if (value.value() == constants::values::kRunAsNonExpectedWorkRequest) {
+    if (value.value() == kRunAsNonExpectedWorkRequest) {
         return OutOfQuotaPolicy::kRunAsNonExpeditedWorkRequest;
-    } else if (value.value() == constants::values::kDropWorkRequest) {
+    } else if (value.value() == kDropWorkRequest) {
         return OutOfQuotaPolicy::kDropWorkRequest;
     }
 
@@ -85,25 +117,25 @@ std::optional<OutOfQuotaPolicy> ExtractOutOfQuotaPolicyFromMap(
 }
 
 NetworkType ExtractNetworkTypeFromMap(const flutter::EncodableMap &args) {
-    std::optional<std::string> value = GetOrNullFromEncodableMap<std::string>(
-        &args, constants::keys::kNetworkTypekey);
+    std::optional<std::string> value =
+        GetOrNullFromEncodableMap<std::string>(&args, kNetworkTypekey);
     if (!value.has_value()) {
         return NetworkType::kNotRequired;
     }
 
     Upper(value.value());
 
-    if (value.value() == constants::values::kConnected) {
+    if (value.value() == kConnected) {
         return NetworkType::kConnected;
-    } else if (value.value() == constants::values::kMetered) {
+    } else if (value.value() == kMetered) {
         return NetworkType::kMetered;
-    } else if (value.value() == constants::values::kNotRequired) {
+    } else if (value.value() == kNotRequired) {
         return NetworkType::kNotRequired;
-    } else if (value.value() == constants::values::kNotRoaming) {
+    } else if (value.value() == kNotRoaming) {
         return NetworkType::kNotRoaming;
-    } else if (value.value() == constants::values::kUnmetered) {
+    } else if (value.value() == kUnmetered) {
         return NetworkType::kUnmetered;
-    } else if (value.value() == constants::values::kTemporarilyUnmetered) {
+    } else if (value.value() == kTemporarilyUnmetered) {
         return NetworkType::kTemporarilyUnmetered;
     }
 
@@ -113,18 +145,14 @@ NetworkType ExtractNetworkTypeFromMap(const flutter::EncodableMap &args) {
 Constraints ExtractConstraintConfigFromMap(const flutter::EncodableMap &map) {
     NetworkType requested_network_type = ExtractNetworkTypeFromMap(map);
     bool requires_battery_not_low =
-        GetOrNullFromEncodableMap<bool>(&map,
-                                        constants::keys::kBatteryNotLowKey)
+        GetOrNullFromEncodableMap<bool>(&map, kBatteryNotLowKey)
             .value_or(false);
     bool requires_charging =
-        GetOrNullFromEncodableMap<bool>(&map, constants::keys::kChargingKey)
-            .value_or(false);
+        GetOrNullFromEncodableMap<bool>(&map, kChargingKey).value_or(false);
     bool requires_device_idle =
-        GetOrNullFromEncodableMap<bool>(&map, constants::keys::kDeviceidlekey)
-            .value_or(false);
+        GetOrNullFromEncodableMap<bool>(&map, kDeviceidlekey).value_or(false);
     bool requires_storage_not_low =
-        GetOrNullFromEncodableMap<bool>(&map,
-                                        constants::keys::kStorageNotLowKey)
+        GetOrNullFromEncodableMap<bool>(&map, kStorageNotLowKey)
             .value_or(false);
 
     return Constraints(requested_network_type, requires_battery_not_low,
--- tizen/src/workmanager_tizen_plugin.cc
+++ tizen/src/workmanager_tizen_plugin.cc
@@ -22,26 +22,71 @@ typedef flutter::MethodCall<flutter::EncodableValue> FlMethodCall;
 typedef flutter::MethodResult<flutter::EncodableValue> FlMethodResult;
 typedef flutter::MethodChannel<flutter::EncodableValue> FlMethodChannel;
 
+const char *kForegroundChannelName =
+    "be.tramckrijte.workmanager/foreground_channel_work_manager";
+const char *kBackgroundChannelName =
+    "be.tramckrijte.workmanager/background_channel_work_manager";
+
+const char *kInitialize = "initialize";
+const char *kRegisterOneOffTask = "registerOneOffTask";
+const char *kRegisterPeriodicTask = "registerPeriodicTask";
+const char *kCancelTaskByUniqueName = "cancelTaskByUniqueName";
+const char *kCancelTaskByTag = "cancelTaskBytag";
+const char *kCancelAllTasks = "cancelAllTasks";
+const char *kUnknown = "unknown";
+
+const char *kOnResultSendMethod = "onResultSend";
+const char *kBackgroundChannelInitialized = "backgroundChannelInitialized";
+
+const char *kIsInDebugModeKey = "isInDebugMode";
+const char *kCallhandlekey = "callbackHandle";
+const char *kFrequencySecondsKey = "frequency";
+const char *kCancelTaskTagKey = "tag";
+const char *kCancelTaskUniqueNameKey = "uniqueName";
+
+const char *kUniquenameKey = "uniqueName";
+const char *kNameValueKey = "taskName";
+const char *kTagKey = "tag";
+
+const char *kBgChannelInputDataKey = "be.tramckrijte.workmanager.INPUT_DATA";
+const char *kBgChannelDartTaskKey = "be.tramckrijte.workmanager.DART_TASK";
+const char *kDispatcherHandleKey = "WMANAGER_TIZEN_DISPATCHER_HANDLE_KEY";
+
+const char *kPayloadKey = "inputData";
+
+const char *kNotInitializedErrMsg =
+    "You have not properly initialized the Flutter WorkManager Package. "
+    "You should ensure you have called the 'initialize' function first! "
+    "Example: \n"
+    "\n"
+    "`Workmanager().initialize(\n"
+    "  callbackDispatcher,\n"
+    " )`"
+    "\n"
+    "\n"
+    "The `callbackDispatcher` is a top level function. See example in "
+    "repository.";
+
 class WorkmanagerTizenPlugin : public flutter::Plugin {
    public:
-    static void RegisterWithRegistrar(flutter::PluginRegistrar* registrar) {
+    static void RegisterWithRegistrar(flutter::PluginRegistrar *registrar) {
         auto plugin = std::make_unique<WorkmanagerTizenPlugin>();
 
         auto foreground_channel = std::make_unique<FlMethodChannel>(
-            registrar->messenger(), constants::kForegroundChannelName,
+            registrar->messenger(), kForegroundChannelName,
             &flutter::StandardMethodCodec::GetInstance());
 
         plugin->background_channel_ = std::make_unique<FlMethodChannel>(
-            registrar->messenger(), constants::kBackgroundChannelName,
+            registrar->messenger(), kBackgroundChannelName,
             &flutter::StandardMethodCodec::GetInstance());
 
         foreground_channel->SetMethodCallHandler(
-            [plugin_pointer = plugin.get()](const auto& call, auto result) {
+            [plugin_pointer = plugin.get()](const auto &call, auto result) {
                 plugin_pointer->HandleWorkmanagerCall(call, std::move(result));
             });
 
         plugin->background_channel_->SetMethodCallHandler(
-            [plugin_pointer = plugin.get()](const auto& call, auto result) {
+            [plugin_pointer = plugin.get()](const auto &call, auto result) {
                 plugin_pointer->HandleBackground(call, std::move(result));
             });
 
@@ -55,13 +100,13 @@ class WorkmanagerTizenPlugin : public flutter::Plugin {
    private:
     std::unique_ptr<FlMethodChannel> background_channel_;
 
-    void HandleWorkmanagerCall(const FlMethodCall& call,
+    void HandleWorkmanagerCall(const FlMethodCall &call,
                                std::unique_ptr<FlMethodResult> result) {
         const auto method_name = call.method_name();
-        const auto& arguments = *call.arguments();
+        const auto &arguments = *call.arguments();
         JobScheduler job_scheduler;
 
-        if (call.method_name() == constants::methods::kCancelAllTasks) {
+        if (call.method_name() == kCancelAllTasks) {
             job_scheduler.CancelAll();
 
             result->Success();
@@ -76,36 +121,33 @@ class WorkmanagerTizenPlugin : public flutter::Plugin {
 
         flutter::EncodableMap map = std::get<flutter::EncodableMap>(arguments);
 
-        if (method_name == constants::methods::kInitialize) {
-            bool isDebugMode = std::get<bool>(map[flutter::EncodableValue(
-                constants::keys::kIsInDebugModeKey)]);
-            int64_t handle = std::get<int64_t>(
-                map[flutter::EncodableValue(constants::keys::kCallhandlekey)]);
-            const auto& call = InitializeTaskInfo(handle, isDebugMode);
+        if (method_name == kInitialize) {
+            bool isDebugMode =
+                std::get<bool>(map[flutter::EncodableValue(kIsInDebugModeKey)]);
+            int64_t handle =
+                std::get<int64_t>(map[flutter::EncodableValue(kCallhandlekey)]);
+            const auto &info = InitializeTaskInfo(handle, isDebugMode);
 
-            preference_set_int(constants::keys::kDispatcherHandleKey,
-                               call.callback_dispathcer_handler_key);
+            preference_set_int(kDispatcherHandleKey,
+                               info.callback_dispathcer_handler_key);
             result->Success();
-        } else if (method_name == constants::methods::kRegisterOneOffTask ||
-                   method_name == constants::methods::kRegisterPeriodicTask) {
-            bool is_debug_mode = std::get<bool>(map[flutter::EncodableValue(
-                constants::keys::kIsInDebugModeKey)]);
+        } else if (method_name == kRegisterOneOffTask ||
+                   method_name == kRegisterPeriodicTask) {
+            bool is_debug_mode =
+                std::get<bool>(map[flutter::EncodableValue(kIsInDebugModeKey)]);
             std::string unique_name = std::get<std::string>(
-                map[flutter::EncodableValue(constants::keys::kUniquenameKey)]);
+                map[flutter::EncodableValue(kUniquenameKey)]);
             std::string task_name = std::get<std::string>(
-                map[flutter::EncodableValue(constants::keys::kNameValueKey)]);
+                map[flutter::EncodableValue(kNameValueKey)]);
             std::optional<std::string> tag =
-                GetOrNullFromEncodableMap<std::string>(
-                    &map, constants::keys::kTagKey);
+                GetOrNullFromEncodableMap<std::string>(&map, kTagKey);
             ExistingWorkPolicy existing_work_policy =
                 ExtractExistingWorkPolicyFromMap(map);
-            int32_t initial_delay_seconds =
-                GetOrNullFromEncodableMap<int32_t>(
-                    &map, constants::keys::kInitialDelaySecondsKey)
-                    .value_or(0);
+            int32_t initial_delay_seconds = GetOrNullFromEncodableMap<int32_t>(
+                                                &map, kInitialDelaySecondsKey)
+                                                .value_or(0);
             int32_t frequency_seconds =
-                GetOrNullFromEncodableMap<int32_t>(
-                    &map, constants::keys::kFrequencySecondsKey)
+                GetOrNullFromEncodableMap<int32_t>(&map, kFrequencySecondsKey)
                     .value_or(0);
 
             Constraints constraints_config =
@@ -116,27 +158,25 @@ class WorkmanagerTizenPlugin : public flutter::Plugin {
                 ExtractBackoffPolicyConfigFromMap(map,
                                                   TaskType(TaskType::kOneOff));
             std::optional<std::string> payload =
-                GetOrNullFromEncodableMap<std::string>(
-                    &map, constants::keys::kPayloadKey);
+                GetOrNullFromEncodableMap<std::string>(&map, kPayloadKey);
 
             LOG_DEBUG("freq=%ld tag=%s payload=%s", frequency_seconds,
                       tag.value_or("no tag").c_str(),
                       payload.value_or("no payload").c_str());
 
-            const auto& call = RegisterTaskInfo(
+            const auto &info = RegisterTaskInfo(
                 is_debug_mode, unique_name, task_name, existing_work_policy,
                 initial_delay_seconds, constraints_config,
                 backoff_policy_config, out_of_quota_policy, frequency_seconds,
                 tag, payload);
-            const bool isPeriodic =
-                method_name == constants::methods::kRegisterPeriodicTask;
+            const bool isPeriodic = method_name == kRegisterPeriodicTask;
 
-            job_scheduler.RegisterJob(call, isPeriodic);
+            job_scheduler.RegisterJob(info, isPeriodic);
 
             result->Success();
-        } else if (method_name == constants::methods::kCancelTaskByUniqueName) {
+        } else if (method_name == kCancelTaskByUniqueName) {
             auto name = GetOrNullFromEncodableMap<std::string>(
-                &map, constants::keys::kCancelTaskUniqueNameKey);
+                &map, kCancelTaskUniqueNameKey);
             if (!name.has_value()) {
                 result->Error("WRONG ARGS", "No name provided");
                 return;
@@ -148,9 +188,9 @@ class WorkmanagerTizenPlugin : public flutter::Plugin {
 
             result->Success();
 
-        } else if (method_name == constants::methods::kCancelTaskByTag) {
-            auto tag = GetOrNullFromEncodableMap<std::string>(
-                &map, constants::keys::kCancelTaskTagKey);
+        } else if (method_name == kCancelTaskByTag) {
+            auto tag =
+                GetOrNullFromEncodableMap<std::string>(&map, kCancelTaskTagKey);
             if (!tag.has_value()) {
                 result->Error("WRONG ARGS", "No name provided");
                 return;
@@ -167,33 +207,30 @@ class WorkmanagerTizenPlugin : public flutter::Plugin {
         }
     }
 
-    void HandleBackground(const FlMethodCall& call,
+    void HandleBackground(const FlMethodCall &call,
                           std::unique_ptr<FlMethodResult> result) {
         LOG_DEBUG("Background call name =%s", call.method_name().c_str());
 
-        if (call.method_name() ==
-            constants::methods::kBackgroundChannelInitialized) {
-            const auto& args = *call.arguments();
+        if (call.method_name() == kBackgroundChannelInitialized) {
+            const auto &args = *call.arguments();
             if (!std::holds_alternative<flutter::EncodableMap>(args)) {
                 result->Error("WRONG ARGS",
                               "No proper argument provided for background");
             }
             flutter::EncodableMap map = std::get<flutter::EncodableMap>(args);
-            auto dart_task = map[flutter::EncodableValue(
-                constants::keys::kBgChannelDartTaskKey)];
-            auto input_data = map[flutter::EncodableValue(
-                constants::keys::kBgChannelInputDataKey)];
+            auto dart_task =
+                map[flutter::EncodableValue(kBgChannelDartTaskKey)];
+            auto input_data =
+                map[flutter::EncodableValue(kBgChannelInputDataKey)];
 
             flutter::EncodableMap arg = {
-                {flutter::EncodableValue(
-                     std::string(constants::keys::kBgChannelDartTaskKey)),
+                {flutter::EncodableValue(std::string(kBgChannelDartTaskKey)),
                  dart_task},
-                {flutter::EncodableValue(
-                     std::string(constants::keys::kBgChannelInputDataKey)),
+                {flutter::EncodableValue(std::string(kBgChannelInputDataKey)),
                  input_data}};
 
             background_channel_->InvokeMethod(
-                constants::methods::kOnResultSendMethod,
+                kOnResultSendMethod,
                 std::make_unique<flutter::EncodableValue>(arg));
         }
     }
